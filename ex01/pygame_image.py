291f812320f133110b8f341a8d8a2413cc4399af
katchi <c0b2208738@edu.teu.ac.jp>
2023-11-21 15:34:53 +0900

    finish

--- ex01/pygame_image.py
+++ ex01/pygame_image.py
@@ -7,23 +7,30 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    rev_bg = pg.transform.flip(bg_img, True, False)
+    bgs = [bg_img, rev_bg]
+    # img_rct = bg_img.get_rect()
+    # img_rct.center = 800, 600
     kokaton = pg.image.load("ex01/fig/3.png")
     kokaton = pg.transform.flip(kokaton, True, False)
-    kokatons = [pg.transform.rotozoom(kokaton, i, 1) for i in range(10)]
-    kokatons += [pg.transform.rotozoom(i, -1, 1) for i in reversed(kokatons)]
+    kokatons = [pg.transform.rotozoom(kokaton, i*0.3, 1) for i in range(50)]
+    kokatons += [pg.transform.rotozoom(i, -0.3, 1) for i in reversed(kokatons)]
     tmr = 0
+    cnt = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bg_img, [1600, 0])
-        screen.blit(kokatons[tmr//10%20], [300, 200])
+        screen.blit(bgs[cnt], (-tmr,0))
+        screen.blit(bgs[~cnt], (1600-tmr,0))
+        screen.blit(kokatons[tmr%100], [300, 200])
         pg.display.update()
-        bg_img.scroll(tmr%1600,0)
-
-        tmr += 1        
-        clock.tick(50)
+        # img_rct.move_ip(1, 0)
+        tmr += 1
+        if tmr >= 1600:
+            tmr = 0
+            cnt = ~cnt
+        clock.tick(500)
 
 
 if __name__ == "__main__":
