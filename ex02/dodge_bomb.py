12252928cdc90a63087365984b0d4c7abe5b0051
katchi <c0b2208738@edu.teu.ac.jp>
2023-11-28 13:05:20 +0900

    ren2

--- ex02/dodge_bomb.py
+++ ex02/dodge_bomb.py
@@ -9,15 +9,16 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     fps = 50
     vx = vy = 5
+    key_lst = {pg.K_UP:(0, -5), pg.K_DOWN:(0, 5), pg.K_LEFT:(-5, 0), pg.K_RIGHT:(5, 0)}
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect()
     bomb_sfc = pg.Surface((40, 40))
     bomb_sfc.set_colorkey((0,0,0))
     bomb = pg.draw.circle(bomb_sfc, (255,0,0), (20,20), 10)
-    bomx = randint(0,1600)
-    bomy = randint(0,800)
+    bomb.center = (randint(0,1600), randint(0,800))
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -25,13 +26,14 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed()
+
+
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bomb_sfc, [bomx, bomy])
+        screen.blit(bomb_sfc, bomb)
         pg.display.update()
-        bomx += vx
-        bomy += vy
-        bomb.move_ip(bomx, bomy)
+        bomb.move_ip((vx,vy))
         tmr += 1
         clock.tick(fps)
 
