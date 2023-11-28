9e79e9b0c7cfc8b830d081fb74b0800ceded2d21
katchi <c0b2208738@edu.teu.ac.jp>
2023-11-28 12:40:39 +0900

    ren2

--- ex02/dodge_bomb.py
+++ ex02/dodge_bomb.py
@@ -1,6 +1,6 @@
 import sys
 import pygame as pg
-
+from random import randint
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -12,7 +12,11 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bomb = 
+    bomb_sfc = pg.Surface((40, 40))
+    bomb_sfc.set_colorkey((0,0,0))
+    bomb = pg.draw.circle(bomb_sfc, (255,0,0), (20,20), 10)
+    bomx = randint(0,1600)
+    bomy = randint(0,800)
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -22,6 +26,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb_sfc, [bomx, bomy])
         pg.display.update()
         tmr += 1
         clock.tick(fps)
