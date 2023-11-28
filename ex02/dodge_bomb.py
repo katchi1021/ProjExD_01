a435e6bfe7f4d701d626f196c5d40d7d4d8dae68
katchi <c0b2208738@edu.teu.ac.jp>
2023-11-28 12:37:26 +0900

    ren1

---
+++ ex02/dodge_bomb.py
@@ -0,0 +1,34 @@
+import sys
+import pygame as pg
+
+
+WIDTH, HEIGHT = 1600, 900
+
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    fps = 50
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bomb = 
+    clock = pg.time.Clock()
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [900, 400])
+        pg.display.update()
+        tmr += 1
+        clock.tick(fps)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
